3c7945934e5a4d9eda09bf2a2a04f162423f728f
Jason-Glover <88393203+Jason-Glover@users.noreply.github.com>
2021-10-02 01:26:38 -0500

    revert changes

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import os
 from flask import Flask, render_template, request, redirect, send_file
-from s3_functions import create_bucket, filter_bucket, list_bucket, list_files, upload_file, show_image
+from s3_functions import list_files, upload_file, show_image
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
-try:
-    BUCKET = os.environ['BUCKET_NAME']
-except:
-    ALLBUCKETS = list_bucket()
-    BUCKET = filter_bucket(ALLBUCKETS)
-    if BUCKET == None:
-        create_bucket()
-        BUCKET = list_bucket
-        
+BUCKET = "img-mgr-docker-6440"
 
 @app.route("/")
 def home():
@@ -32,9 +24,7 @@ def upload():
         f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
         upload_file(f"uploads/{f.filename}", BUCKET)
         return render_template('index.html')
-@app.route('/verify')
-def verify():
-        return '<p>' + BUCKET + '</p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
